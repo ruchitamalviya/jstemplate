dbe13da379337300df0ac6c190da71d91add164b
ruchitamalviya <88784758+ruchitamalviya@users.noreply.github.com>
2021-08-31 14:57:53 +0530

    Add files via upload
    change

--- assests/custom.js
+++ assests/custom.js
@@ -1,23 +1,41 @@
-	var color=["blue","green","red"];
-
-	length=color.length;
-
+	
+	var color=["#fc032c","#3168de","#46de28"];
+	var length=color.length;
 	var i=0;
 
-	document.getElementById('item1').addEventListener("click",fillcolor);
-	document.getElementById('item2').addEventListener("click",fillcolor);
-	document.getElementById('item3').addEventListener("click",fillcolor);
-
+	document.getElementById("item1").addEventListener('click', fillcolor);
+	document.getElementById("item2").addEventListener('click', fillcolor);
+	document.getElementById("item3").addEventListener('click', fillcolor);
 
 	function fillcolor(){
+	i = (i<length-1) ? ++i :0;
 
-	i=i<length ? ++i :0;
-
-	document.getElementById("item1").style.background=color[i];
-	document.getElementById("item2").style.background=color[i];
-	document.getElementById("item3").style.background=color[i];
-
+	document.getElementById('item1').style.background=color[i];
+	document.getElementById('item2').style.background=color[i];
+	document.getElementById('item3').style.background=color[i];
 	}
+
+	
+
+
+
+
+
+
+
+
+
+	
+	
+
+
+	
+
+	
+
+
+	
+
 
 	
 
