830e15eafe07dfb57b7f9a49d89a991a1ebef842
ruchitamalviya <88784758+ruchitamalviya@users.noreply.github.com>
2021-09-02 10:26:20 +0530

    Add files via upload
    newadded

--- assests/custom.js
+++ assests/custom.js
@@ -1,19 +1,27 @@
 	
-	var color=["#fc032c","#3168de","#46de28"];
-	var length=color.length;
-	var i=0;
-
+	function addbox() {
+		var addbox = document.createElement("DIV");
+		addbox.className = 'item';
+		addbox.onclick=fillcolor;
+		document.body.appendChild(addbox);
+	}
+	
+	var color = ["#fc032c", "#3168de", "#46de28"];
+	var length = color.length;
+	var i = 0;
 	document.getElementById("item1").addEventListener('click', fillcolor);
 	document.getElementById("item2").addEventListener('click', fillcolor);
 	document.getElementById("item3").addEventListener('click', fillcolor);
+	document.getElementsByClassName("item").addEventListener('click', fillcolor);
 
-	function fillcolor(){
-	i = (i<length-1) ? ++i :0;
-
-	document.getElementById('item1').style.background=color[i];
-	document.getElementById('item2').style.background=color[i];
-	document.getElementById('item3').style.background=color[i];
+	function fillcolor() {
+		i = (i < length - 1) ? ++i : 0;
+		document.getElementById('item1').style.background = color[i];
+		document.getElementById('item2').style.background = color[i];
+		document.getElementById('item3').style.background = color[i];
+		getElementsByClassName('item').style.background = color[i];//error
 	}
+
 
 	
 
