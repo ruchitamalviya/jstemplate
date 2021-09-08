383824af3eacead851f8678c959c37b44a213f36
ruchitamalviya <88784758+ruchitamalviya@users.noreply.github.com>
2021-09-08 19:42:50 +0530

    Add files via upload
    newadded

--- assests/custom.js
+++ assests/custom.js
@@ -1,31 +1,22 @@
-function addBox()
-{
-	
-	 var addbox = document.createElement("DIV");
-	addbox.className='item';
-	document.getElementById('parent').appendChild(addbox);
-	
+(function() {
+	let button = document.getElementById("addbox");
+	button.onclick = function() {
+		let addbox = document.createElement("DIV");
+		addbox.className = 'item';
+		document.getElementById('parent').appendChild(addbox);
 	}
 
-(function() {
-let count=0;
-
-
-	document.addEventListener('click',function(e){
+	document.addEventListener('click', function(e) {
 		element = document.getElementsByClassName("item");
-		
-		let color = ["#fc032c", "#3168de", "#46de28"];
-		for ( let i = 0; i < element.length; i++ ) {
-			var divElm = element[i];
-			 divElm.onclick = function() {
-			 	let count1 = count % 3;
-			 	console.log(color[0]);
-				this.style.background = color[count1];
-				count++;
-		    }
+		let color = ["blue", "green", "red"];
+		for(let i = 0; i < element.length; i++) {
+			let divElm = element[i];
+			divElm.onclick = function(eleClick) {
+				let divTarget 	 = eleClick.target;
+				divTarget.clicks = (divTarget.clicks || 0) + 1;
+				let clickCcount  = divTarget.clicks % 3;
+				this.style.background = color[clickCcount];
+			}
 		}
 	});
-
-
 })();
-
